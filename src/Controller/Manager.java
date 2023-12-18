9d93c3c4d9be7c18acbd5d64593281ee6c2820e4
Md Numan Hussain <mh21adq@herts.ac.uk>
2023-12-18 04:32:20 +0000

    Introduce competitorList attribute in Manager class for managing competitors

--- src/Controller/Manager.java
+++ src/Controller/Manager.java
@@ -1,4 +1,12 @@
 package Controller;
-
+import Model.CompetitorList;
+import Model.Name;
+import Model.Level;
+import Model.Competitor;
+import Model.IceSkater;
+import Model.Gamer;
+import java.io.*;
+import java.util.*;
 public class Manager {
+    private CompetitorList competitorList = new CompetitorList();
 }
