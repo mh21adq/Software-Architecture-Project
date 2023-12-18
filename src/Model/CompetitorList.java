53b3928d28f60e6e8ff6afcc8c1524a83bdffe6f
Md Numan Hussain <mh21adq@herts.ac.uk>
2023-12-18 04:22:47 +0000

    Implement addCompetitor and removeCompetitor methods in CompetitorList class

--- src/Model/CompetitorList.java
+++ src/Model/CompetitorList.java
@@ -1,4 +1,17 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class CompetitorList {
+    private static ArrayList<Competitor> competitors = new ArrayList<>();
+
+    // Method to add a competitor
+    public void addCompetitor(Competitor competitor) {
+        CompetitorList.competitors.add(competitor);
+    }
+
+    // Method to remove a competitor
+    public void removeCompetitor(Competitor competitor) {
+        CompetitorList.competitors.remove(competitor);
+    }
 }
