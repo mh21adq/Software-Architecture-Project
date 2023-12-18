a0d9156aa4ae37111e33933484e3c534a8eecaf5
Md Numan Hussain <mh21adq@herts.ac.uk>
2023-12-18 03:53:50 +0000

    Add basic attributes to abstract Competitor class

--- src/Model/Competitor.java
+++ src/Model/Competitor.java
@@ -1,4 +1,13 @@
 package Model;
 
 public abstract class Competitor {
+    private static int lastNumber = 99;
+
+    private int competitorNumber;
+    private Name name;
+    private String email;
+    private int age;
+    private String gender;
+    private String country;
+    private String level;
 }
