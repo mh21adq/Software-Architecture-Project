c3e5bcc128f5f701ec8f6911245ee8ce787cce1c
Md Numan Hussain <mh21adq@herts.ac.uk>
2023-12-18 03:45:55 +0000

    Add getters, setters, and full name concatenation to Name class

--- src/Model/Name.java
+++ src/Model/Name.java
@@ -21,4 +21,35 @@ public class Name {
     }
 
 
+
+    // Getters and setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getFullName() {
+        return this.firstName + " " + this.middleName + " " + this.surname;
+    }
+
+
 }
