976333b276f64ede7a1f0265ab48a3f106938bbb
Md Numan Hussain <mh21adq@herts.ac.uk>
2023-12-18 03:44:25 +0000

    Implement overloaded constructors for Name class to support various name formats

--- src/Model/Name.java
+++ src/Model/Name.java
@@ -1,4 +1,24 @@
 package Model;
-
 public class Name {
+    private String firstName="";
+    private String middleName="";
+    private String surname="";
+
+    // Constructors
+    public Name(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Name(String firstName, String surname) {
+        this.firstName = firstName;
+        this.surname = surname;
+    }
+
+    public Name(String firstName, String middleName, String surname) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.surname = surname;
+    }
+
+
 }
