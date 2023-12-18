3a1f957869f9e788e68e01a766f7442b713c181d
Md Numan Hussain <mh21adq@herts.ac.uk>
2023-12-18 03:55:58 +0000

    Add getters and setters for Competitor class attributes

--- src/Model/Competitor.java
+++ src/Model/Competitor.java
@@ -18,4 +18,53 @@ public abstract class Competitor {
         this.gender = gender;
         this.country = country;
     }
+    public int getCompetitorNumber() {
+        return competitorNumber;
+    }
+
+    public void setCompetitorNumber(int competitorNumber) {
+        this.competitorNumber = competitorNumber;
+    }
+
+    public String getName() {
+        return this.name.getFullName();
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+
 }
